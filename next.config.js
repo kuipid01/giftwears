185be1c11b9c55f312e141a5eb37c17bd5b8ad5e
kuipid01 <kuipid01@gmnail.com>
2024-01-14 23:37:44 +0100

    yesting scroll restoratiom

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
-
-module.exports = nextConfig
+export const experimental = {
+    scrollRestoration: true,
+};
+export default nextConfig
