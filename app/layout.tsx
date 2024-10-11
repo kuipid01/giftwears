36f6a6225c2e829932e303622fc13944afe26575
kuipid01 <kuipid01@gmail.com>
2024-10-11 16:46:07 +0100

    at last

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Nav from "./components/Header";
 import Footer from "./components/Footer";
 import { Toaster } from "@/components/ui/toaster";
+import { getApp, initializeApp } from "firebase/app";
 const instru = Instrument_Serif({
   weight: "400",
   subsets: ["latin"],
@@ -13,7 +14,17 @@ export const metadata: Metadata = {
   title: "Gift Wears",
   description: "Gift wears",
 };
+// Your web app's Firebase configuration
 
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 export default function RootLayout({
   children,
 }: {
